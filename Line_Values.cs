e63590f96e095a576a2ea16bc736cd9077528bd9
Satantra Tewari <satantra1508@gmail.com>
2020-10-05 01:34:13 +0530

    [satantra] length of line calculated

---
+++ Line_Values.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Line_Comparison_Problem
+{
+    class Line_Values
+    {
+        public double xcoOrdinate1;
+        public double ycoOrdinate1;
+        public double xcoOrdinate2;
+        public double ycoOrdinate2;
+        public Line_Values(double xcoOrdinate1, double ycoOrdinate1, double xcoOrdinate2, double ycoOrdinate2)
+        {
+            this.xcoOrdinate1 = xcoOrdinate1;
+            this.ycoOrdinate1 = ycoOrdinate1;
+            this.xcoOrdinate2 = xcoOrdinate2;
+            this.ycoOrdinate2 = ycoOrdinate2;
+        }
+        public double LineLength(double xcoOrdinate1, double ycoOrdinate1, double xcoOrdinate2, double ycoOrdinate2)
+        {
+            double lineLength = Math.Sqrt(Math.Pow(xcoOrdinate2 - xcoOrdinate1, 2) + Math.Pow(ycoOrdinate2 - ycoOrdinate1, 2));
+            return lineLength;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ namespace Line_Comparison_Problem
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Line Comparison Computation Program ");
+            Console.WriteLine(" Enter the X and Y co-ordinates of starting point of line: ");
+            double abscissa1 = Double.Parse(Console.ReadLine());
+            double ordinate1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine(" Enter the X and Y co-ordinates of ending point of line: ");
+            double abscissa2 = Double.Parse(Console.ReadLine());
+            double ordinate2 = Double.Parse(Console.ReadLine());
+            Line_Values lineValues = new Line_Values(abscissa1, ordinate1, abscissa2, ordinate2);
+            Console.WriteLine(" Length of the line is: " + lineValues.LineLength(abscissa1, ordinate1, abscissa2, ordinate2));
         }
     }
 }
