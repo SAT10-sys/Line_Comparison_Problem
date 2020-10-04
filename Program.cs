5f3fb50ada8e3acf68fa92a13a3f6b474ebd1a36
Satantra Tewari <satantra1508@gmail.com>
2020-10-05 02:02:24 +0530

    [satantra] length of two lines calculated

--- Program.cs
+++ Program.cs
@@ -6,15 +6,27 @@ namespace Line_Comparison_Problem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Welcome to Line Comparison Computation Program ");
-            Console.WriteLine(" Enter the X and Y co-ordinates of starting point of line: ");
-            double abscissa1 = Double.Parse(Console.ReadLine());
-            double ordinate1 = Double.Parse(Console.ReadLine());
-            Console.WriteLine(" Enter the X and Y co-ordinates of ending point of line: ");
-            double abscissa2 = Double.Parse(Console.ReadLine());
-            double ordinate2 = Double.Parse(Console.ReadLine());
-            Line_Values lineValues = new Line_Values(abscissa1, ordinate1, abscissa2, ordinate2);
-            Console.WriteLine(" Length of the line is: " + lineValues.LineLength(abscissa1, ordinate1, abscissa2, ordinate2));
+            double lengthOfLine1 = 0, lengthOfLine2 = 0;
+            Console.WriteLine(" Enter the following values of Line 1 in order: ");
+            Console.WriteLine("\n 1. X co-ordinate of starting point\n 2. Y co-ordinate of starting point\n 3. X co-ordinate of ending point\n 4. Y co-ordinate of ending point\n");
+            double abscissa11 = Double.Parse(Console.ReadLine());
+            double ordinate11= Double.Parse(Console.ReadLine());
+            double abscissa21= Double.Parse(Console.ReadLine());
+            double ordinate21= Double.Parse(Console.ReadLine());
+            Console.WriteLine(" Enter the following values of Line 2 in order: ");
+            Console.WriteLine("\n 1. X co-ordinate of starting point\n 2. Y co-ordinate of starting point\n 3. X co-ordinate of ending point\n 4. Y co-ordinate of ending point\n");
+            double abscissa12 = Double.Parse(Console.ReadLine());
+            double ordinate12 = Double.Parse(Console.ReadLine());
+            double abscissa22 = Double.Parse(Console.ReadLine());
+            double ordinate22 = Double.Parse(Console.ReadLine());
+            Line_Values lineValues1 = new Line_Values(abscissa11, ordinate11, abscissa21, ordinate21);
+            Line_Values lineValues2 = new Line_Values(abscissa12, ordinate12, abscissa22, ordinate22);
+            lengthOfLine1 = lineValues1.LineLength(abscissa11, ordinate11, abscissa21, ordinate21);
+            lengthOfLine2 = lineValues2.LineLength(abscissa12, ordinate12, abscissa22, ordinate22);
+            if (lengthOfLine1 == lengthOfLine2)
+                Console.WriteLine(" Both the lines are of equal length ");
+            else
+                Console.WriteLine(" Both the lines are of unequal length ");
         }
     }
 }
